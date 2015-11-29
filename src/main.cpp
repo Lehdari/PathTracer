ec31f8f12eff1de66236b2dd8d23dca687f74dee
LehdaRi <miika.lehtimaki@gmail.com>
2015-11-29 06:31:25 +0200

    additional test scenes

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,11 @@ int main(void) {
                                   Vector3f{1.0f,  -1.0f,  1.0f},
                                   Vector3f{-1.0f, 1.0f,   1.0f} });*/
     Scene scene;
-    scene.loadFromObj("res/scenes/torus.obj");
+    scene.loadFromObj("res/scenes/torus2.obj");
 
     BasicCamera camera(400, 300, 90.0, 4.0/3.0);
 
-    camera.lookAt( {2.0f, 3.0f, 4.0f},
+    camera.lookAt( {2.0f, 2.5f, 4.0f},
                    {0.0f, 0.0f, 0.0f},
                    {0.0f, 1.0f, 0.0f} );
 
